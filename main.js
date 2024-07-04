b302adeb2fe5526e26fec0ab3b2dfee5b0a8d411
Risodu <riskodudek@gmail.com>
2024-07-04 13:19:32 +0200

    Advanced parsing

--- main.js
+++ main.js
@@ -10,7 +10,9 @@ function update() {
     console.log(mathml)
     let res = processMathML(mathml)
     console.log(res)
-    jsonOutput.innerHTML = JSON.stringify(res, null, 2)
+    console.log(res.toString())
+    // jsonOutput.innerHTML = JSON.stringify(res, null, 2)
+    jsonOutput.innerHTML = res.toString()
 }
 
 function processMathML(element) {
@@ -20,17 +22,98 @@ function processMathML(element) {
         case 'math':
         case 'mrow':
         case 'msup':
+        case 'mfrac':
             let result = []
             for (let i = 0; i < element.childNodes.length; i++) {
-                const child = element.childNodes[i];
+                const child = element.childNodes[i]
                 let part = processMathML(child)
                 if(part != undefined)
                     result.push(part)
             }
-            return result
+            if(element.dataset.semanticType == 'fenced')
+                return result[0]
+            let type = element.dataset.semanticRole
+            if(opAlias[type] != undefined) type = opAlias[type]
+            return new Operation(result, type)
         case 'mi':
+            return new Variable(element.innerHTML)
         case 'mn':
-            return element.innerHTML
+            return new NumberExp(Number(element.innerHTML))
     }
-    console.log(tag, element)
 }
+
+var opAlias = {
+    "implicit": "multiplication"
+}
+
+var operations = {
+    "addition": {
+        "operator": "+"
+    },
+    "multiplication": {
+        "operator": "*"
+    },
+    "subtraction": {
+        "operator": "-"
+    },
+    "division": {
+        "operator": "/"
+    },
+    "equality": {
+        "operator": "="
+    },
+    "negative": {
+        "prefix": "-"
+    }
+}
+
+class Expression {
+    toString() {}
+}
+
+class NumberExp extends Expression {
+    value
+
+    constructor(value) {
+        super()
+        this.value = value
+    }
+
+    toString() {
+        return this.value
+    }
+}
+
+class Variable extends Expression {
+    identifier
+
+    constructor(identifier) {
+        super()
+        this.identifier = identifier
+    }
+
+    toString() {
+        return this.identifier
+    }
+}
+
+class Operation extends Expression {
+    elements
+    opType
+
+    constructor(elements, type) {
+        super()
+        this.elements = elements
+        this.opType = type
+    }
+    
+    toString() {
+        let result = '('
+        for (let i = 0; i < this.elements.length; i++) {
+            if(i) result += operations[this.opType].operator
+            result += this.elements[i].toString()
+        }
+        return result + ')'
+    }
+}
+
